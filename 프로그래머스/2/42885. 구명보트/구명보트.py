9be3f88c5183fdc4d28718e90ad83d147fe0ca4a
Jisu Lee <dlwltn0430@naver.com>
2025-09-07 17:15:56 +0900

    [level 2] Title: 구명보트, Time: 10.02 ms, Memory: 9.62 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,16 @@
+def solution(people, limit):
+    cnt = 0 
+    people.sort()
+    start = 0
+    end = len(people) - 1
+    
+    while (start <= end):
+        if people[start] + people[end] <= limit:
+            start += 1
+            end -= 1            
+        else:
+            end -= 1
+   
+        cnt += 1
+    
+    return cnt
